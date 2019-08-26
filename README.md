2f4e25f30ba4a7327649ae3d1cdf8b1aa7bc1610
Stefan Oderbolz <oderbolz@gmail.com>
2019-08-26 17:07:17 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,61 @@
+
+
+# sruthie
+
+**sru**thie is a client for python to make [SRU requests (Search/Retrieve via URL)](http://www.loc.gov/standards/sru/).
+
+## Contents
+
+* [Usage](#usage)
+* [Schemas](#schemas)
+
+## Usage
+
+```python
+import sruthie
+
+records = sruthie.search('https://suche.staatsarchiv.djiktzh.ch/SRU/', query='Zurich')
+
+for record in records:
+    # print fields from schema
+    print(record['reference'])
+    print(record['title'])
+    print(record['date'])
+    print(record['extra']['link']) # extra record data is available at the 'extra' key
+```
+
+```python
+# you can get more information at each step
+import sruthie
+
+# note: records is an iterator
+records = sruthie.search('https://suche.staatsarchiv.djiktzh.ch/SRU/', query='Human')
+print(records.cql)
+print(records.sru_version)
+print(records.schema)
+print(records.count)
+
+for record in records:
+    print(record)
+    print(record.schema)
+```
+
+```python
+import sruthie
+
+info = sruthie.explain('https://suche.staatsarchiv.djiktzh.ch/SRU/')
+print(info.server)
+print(info.database)
+print(info.index)
+print(info.schema)
+```
+
+
+
+## Schemas
+
+Currently the following schemas are supported by sruthie. Altough 
+
+* [Dublin Core Record Schema](http://www.loc.gov/standards/sru/recordSchemas/dc-schema.html) (dc)
+* [MARCXML: The MARC 21 XML Schema](http://www.loc.gov/standards/marcxml/schema/MARC21slim.xsd) (marcxml)
+* [ISAD(G): General International Standard Archival Description, Second edition](http://www.expertisecentrumdavid.be/xmlschemas/isad.xsd) (isad)
---
+++ setup.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+from codecs import open
+from setuptools import setup
+import re
+
+with open('sruthie/__init__.py', 'r') as fd:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        fd.read(), re.MULTILINE).group(1)
+
+if not version:
+    raise RuntimeError('Cannot find version information')
+
+try:
+    import pypandoc
+    from unidecode import unidecode
+    description = open('README.md', encoding='utf-8').read()
+    description = unidecode(description)
+    description = pypandoc.convert(description, 'rst', format='md')
+except (IOError, OSError, ImportError):
+    description = 'SRU client for Python'
+
+setup(
+    name='sruthie',
+    packages=['sruthie'],
+    version=version,
+    install_requires=['requests'],
+    description='SRU client for Python',
+    long_description=description,
+    author='Stefan Oderbolz',
+    author_email='odi@metaodi.ch',
+    maintainer='Stefan Oderbolz',
+    maintainer_email='odi@metaodi.ch',
+    url='https://github.com/metaodi/sruthie',
+    download_url='https://github.com/metaodi/sruthie/archive/v%s.zip' % version,
+    keywords=['sru', 'search', 'retrieve', 'archive', 'library'],
+    license='GPLv3',
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
