f9117eb2407bd91be45fd72fdc35cf08410cf39e
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2020-10-02 13:36:05 +0200

    Pass through parameters from `sru` module

--- tests/sru_test.py
+++ tests/sru_test.py
@@ -7,6 +7,18 @@ class TestSru(SruthiTestCase):
         r = sruthi.searchretrieve('http://test.com/sru/', 'Test-Query')
         self.assertIsInstance(r, sruthi.response.SearchRetrieveResponse)
 
+    def test_searchretrieve_with_maximum_records(self):
+        r = sruthi.searchretrieve('http://test.com/sru/', 'Test-Query', maximum_records=100)
+        self.assertIsInstance(r, sruthi.response.SearchRetrieveResponse)
+
+    def test_searchretrieve_with_record_schema(self):
+        r = sruthi.searchretrieve('http://test.com/sru/', 'Test-Query', record_schema='isad')
+        self.assertIsInstance(r, sruthi.response.SearchRetrieveResponse)
+
+    def test_searchretrieve_with_start_record(self):
+        r = sruthi.searchretrieve('http://test.com/sru/', 'Test-Query', start_record=10)
+        self.assertIsInstance(r, sruthi.response.SearchRetrieveResponse)
+
     def test_explain(self):
         info = sruthi.explain('http://test.com/sru/')
         self.assertIsInstance(info, sruthi.response.ExplainResponse)
